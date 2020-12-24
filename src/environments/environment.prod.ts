39cdbd0114b681263dca2c4fa691bc3ec390b4d3
Pedro Silva <pedrohenriqueuh2010@hotmail.com>
2020-12-24 17:56:32 -0300

    Configurando variaveis de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
     hmr       : false,
-    fgl_api: 'https://fgl-srvc-produtcs-dot-cerc-homolog.appspot.com/api',
-    fgl_occ_api: 'https://fgl-srvc-integration-occ-dot-cerc-homolog.appspot.com/api'
+    api_url: 'https://homolog-challenge-api.herokuapp.com'
 };
