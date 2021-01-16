8658aac21e7af1fb9345cd0e154ba74fb1d6fd2f
Pedro Silva <pedrohenriqueuh2010@hotmail.com>
2021-01-16 20:16:43 -0300

    Configurando base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     hmr       : false,
-    api_url: 'http://hml-challenge90-com.umbler.net',
+    api_url: 'https://hml-challenge90-com.umbler.net',
     // api_url: 'https://homolog-challenge-api.herokuapp.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   hmr: false,
-  // api_url: 'http://hml-challenge90-com.umbler.net',
-  api_url: 'http://localhost:3333',
+  api_url: 'https://hml-challenge90-com.umbler.net',
+  // api_url: 'http://localhost:3333',
   // api_url: 'https://homolog-challenge-api.herokuapp.com'
 };
