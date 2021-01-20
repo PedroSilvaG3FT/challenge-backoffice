802e9fddbbf535a3c97744f6b90f9efc48e226b6
Pedro Silva <pedrohenriqueuh2010@hotmail.com>
2021-01-19 23:02:34 -0300

    Alterando enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
     hmr       : false,
-    api_url: 'https://hml-challenge90-com.umbler.net',
-    // api_url: 'https://homolog-challenge-api.herokuapp.com',
+    api_url: 'http://challenge-api-com.umbler.net',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   hmr: false,
-  // api_url: 'https://hml-challenge90-com.umbler.net',
+  // api_url: 'http://challenge-api-com.umbler.net',
   api_url: 'http://localhost:3333',
-  // api_url: 'https://homolog-challenge-api.herokuapp.com'
 };
