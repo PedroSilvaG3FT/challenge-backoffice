48e7058882f58e3ec74d7691ae9253c00c480ac9
Pedro Silva <pedro.silva@cbyk.com.br>
2021-10-08 15:57:39 -0300

    Atualizando dominio de api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   hmr: false,
-  api_url: 'http://challenge-api-com.umbler.net',
+  api_url: 'https://challenge-api.com',
   // api_url: 'http://localhost:3333',
 };
