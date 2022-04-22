f73d5f5ce5fe4d3c84ea96fa86db3138df2e4a7a
Pedro Silva <pedro.silva@cbyk.com.br>
2022-04-22 14:04:48 -0300

    Get member approval

--- src/app/modules/member/pages/member-approval/member-approval.component.ts
+++ src/app/modules/member/pages/member-approval/member-approval.component.ts
@@ -1,76 +1,79 @@
-import { Component, OnInit } from '@angular/core';
-import { MemberInterface } from '../../interfaces/member.interface';
-import { PaymentUserService } from '../../services/payment-user.service';
-import { UserService } from '../../services/user.service';
+import { Component, OnInit } from "@angular/core";
+import { USER_TYPE } from "../../constants/userType.constant";
+import { MemberInterface } from "../../interfaces/member.interface";
+import { PaymentUserService } from "../../services/payment-user.service";
+import { UserService } from "../../services/user.service";
 
 @Component({
-    selector: 'app-member-approval',
-    templateUrl: './member-approval.component.html',
+  selector: "app-member-approval",
+  templateUrl: "./member-approval.component.html",
 })
 export class MemberApprovalComponent implements OnInit {
-    public title: string = "Aprovação de Membros";
+  public title: string = "Aprovação de Membros";
 
-    public displayedColumns: string[] = ['id', 'name', 'startingWeight', 'action'];
-    public dataSource: any[] = [];
+  public displayedColumns: string[] = [
+    "id",
+    "name",
+    "startingWeight",
+    "action",
+  ];
+  public dataSource: any[] = [];
 
-    constructor(
-        private userService: UserService,
-        private paymentUserService: PaymentUserService,
-    ) { }
+  constructor(
+    private userService: UserService,
+    private paymentUserService: PaymentUserService
+  ) {}
 
-    ngOnInit(): void { 
+  ngOnInit(): void {
+    this.setDataSource();
+  }
+
+  setDataSource() {
+    const params = {
+      active: false,
+      userType: USER_TYPE.challenge,
+    };
+
+    this.userService.getAll(params).subscribe(
+      (response) => {
+        this.dataSource = response.filter((user) => !user.payday);
+      },
+      (error) => console.log("ERROR :", error)
+    );
+  }
+
+  approveMember(member: MemberInterface) {
+    const currentDate = new Date();
+
+    member.paymentId = 1;
+    member.active = true;
+    member.dateApproval = currentDate;
+    member.payday = currentDate.getDate();
+
+    this.userService.update(member).subscribe(
+      (response) => {
+        this.paymentUserService
+          .create({
+            userId: member.id,
+            payday: member.payday,
+            paymentId: member.paymentId,
+          })
+          .subscribe(() => this.setDataSource());
+      },
+      (error) => {
+        console.log("ERROR :", error);
+      }
+    );
+  }
+
+  reproveMember(id) {
+    this.userService.reprove(id).subscribe(
+      (response) => {
         this.setDataSource();
-    }
-
-    setDataSource() {
-        const params = {
-            active: false
-        };
-
-        this.userService
-            .getAll(params)
-            .subscribe(
-                response => {
-                    this.dataSource = response.filter(user => !user.payday)
-                },
-                error => console.log("ERROR :", error)
-            )
-    }
-
-    approveMember(member: MemberInterface) {
-        const currentDate = new Date()
-
-        member.paymentId = 1
-        member.active = true;
-        member.dateApproval = currentDate
-        member.payday = currentDate.getDate()
-        
-        this.userService
-            .update(member)
-            .subscribe(
-                response => {
-                    this.paymentUserService.create({
-                        userId: member.id,
-                        payday: member.payday,
-                        paymentId: member.paymentId
-                    }).subscribe(() => this.setDataSource())
-                },
-                error => {
-                    console.log("ERROR :", error);
-                }
-            )
-    }
-
-    reproveMember(id) {
-        this.userService
-            .reprove(id)
-            .subscribe(
-                response => {
-                    this.setDataSource();
-                },
-                error => {
-                    console.log("ERROR :", error);
-                }
-            )
-    }
+      },
+      (error) => {
+        console.log("ERROR :", error);
+      }
+    );
+  }
 }
